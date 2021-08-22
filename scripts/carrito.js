1577095d64bbdc760d754cf8f4188e651b620843
Omar Jesús Maniás <ojmanias1985@gmail.com>
2021-08-22 07:51:42 -0300

    Clear cart Function

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -86,18 +86,14 @@ function removeShoppingCartItem(e) {
 }
 
 
-//sumar precios de productos
 
-
-function sumarPrecios() {
-    let precioTotal = 0;
-
-    for (let i = 0; i < Lista.length; i++) {
-        precioTotal += parseInt(Lista[i].productPrice);
-    }
-    $('#precioTotal').text(precioTotal);
-
+function clearCart() {
+    $('#listado').empty()
+    window.localStorage.clear()
+    Lista = []
 }
+
+$('#acceptBuy').on('click', clearCart)
 
 
 
